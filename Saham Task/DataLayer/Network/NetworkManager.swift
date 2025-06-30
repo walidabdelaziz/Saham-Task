8e3c86e79d62d36ed4d1eb3cbcb5fccfb4b22b6f
walid abdelaziz <49992473+walidabdelaziz@users.noreply.github.com>
2025-06-30 14:30:28 +0300

    add network manager

---
+++ Saham Task/DataLayer/Network/NetworkManager.swift
@@ -0,0 +1,58 @@
+//
+//  NetworkManager.swift
+//  Saham Task
+//
+//  Created by Walid Ahmed on 30/06/2025.
+//
+
+import Foundation
+
+enum HTTPMethod: String {
+    case get = "GET"
+    case post = "POST"
+    case put = "PUT"
+    case delete = "DELETE"
+}
+
+protocol NetworkService {
+    func request<T: Decodable>(
+        method: HTTPMethod,
+        url: String,
+        headers: [String: String],
+        params: [String: Any]?,
+        of type: T.Type
+    ) async throws -> T
+}
+
+actor NetworkManager: NetworkService {
+    func request<T: Decodable>(
+        method: HTTPMethod,
+        url: String,
+        headers: [String: String] = [:],
+        params: [String: Any]?,
+        of type: T.Type
+    ) async throws -> T {
+
+        guard let requestURL = URL(string: url) else {
+            throw URLError(.badURL)
+        }
+
+        var request = URLRequest(url: requestURL)
+        request.httpMethod = method.rawValue
+        request.allHTTPHeaderFields = headers
+
+        if let params = params {
+            request.httpBody = try JSONSerialization.data(withJSONObject: params)
+            request.setValue("application/json", forHTTPHeaderField: "Content-Type")
+        }
+
+        let (data, response) = try await URLSession.shared.data(for: request)
+
+        guard let httpResponse = response as? HTTPURLResponse,
+              200..<300 ~= httpResponse.statusCode else {
+            throw URLError(.badServerResponse)
+        }
+
+        return try JSONDecoder().decode(T.self, from: data)
+    }
+}
