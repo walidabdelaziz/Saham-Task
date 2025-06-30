76b064b4294ad99e172fd71a9faa6be31ddb5668
walid abdelaziz <49992473+walidabdelaziz@users.noreply.github.com>
2025-06-30 15:40:00 +0300

    add constants file

---
+++ Saham Task/Helpers/Constants.swift
@@ -0,0 +1,13 @@
+//
+//  Constants.swift
+//  Saham Task
+//
+//  Created by Walid Ahmed on 30/06/2025.
+//
+
+class Constants{
+    static var BASE_URL = "http://localhost:3001/"
+    
+    // MARK:- Orders
+    static var ORDERS = BASE_URL + "orders"
+}
