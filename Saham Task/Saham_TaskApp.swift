36c80155c9f9f4a63707ec38c2581247a90aac39
walid abdelaziz <49992473+walidabdelaziz@users.noreply.github.com>
2025-06-30 14:11:18 +0300

    Initial Commit

---
+++ Saham Task/Saham_TaskApp.swift
@@ -0,0 +1,17 @@
+//
+//  Saham_TaskApp.swift
+//  Saham Task
+//
+//  Created by Walid Ahmed on 30/06/2025.
+//
+
+import SwiftUI
+
+@main
+struct Saham_TaskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ Saham TaskTests/Saham_TaskTests.swift
@@ -0,0 +1,17 @@
+//
+//  Saham_TaskTests.swift
+//  Saham TaskTests
+//
+//  Created by Walid Ahmed on 30/06/2025.
+//
+
+import Testing
+@testable import Saham_Task
+
+struct Saham_TaskTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
