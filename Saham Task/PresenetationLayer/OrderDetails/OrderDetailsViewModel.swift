9aae13a2cb89b55f332908446d63b4272a3a52f6
walid abdelaziz <49992473+walidabdelaziz@users.noreply.github.com>
2025-06-30 17:21:36 +0300

    add order details view model

--- Saham Task/PresenetationLayer/OrderDetails/OrderDetailsViewModel.swift
+++ Saham Task/PresenetationLayer/OrderDetails/OrderDetailsViewModel.swift
@@ -5,3 +5,26 @@
 //  Created by Walid Ahmed on 30/06/2025.
 //
 
+import SwiftUI
+
+@Observable class OrderDetailsViewModel {
+    var order: Order = Order()
+    var isLoading: Bool = false
+
+    private let orderDetailsService: OrderDetailsService
+    
+    init(orderDetailsService: OrderDetailsService) {
+        self.orderDetailsService = orderDetailsService
+    }
+    @MainActor
+    func getOrderDetails(id: Int) async {
+        isLoading = true
+        do {
+            let result = try await orderDetailsService.getOrderDetails(id: id)
+            order = result
+        } catch {
+            print("Failed to fetch order: \(error)")
+        }
+        isLoading = false
+    }
+}
